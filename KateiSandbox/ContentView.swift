b6b8c9052e0ff9df5dab816ef6cdcd4620187c7c
Yuta Saito <kateinoigakukun@gmail.com>
2019-06-23 12:48:20 +0900

    Initial Commit

---
+++ KateiSandbox/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KateiSandbox
+//
+//  Created by kateinoigakukun on 2019/06/23.
+//  Copyright © 2019 kateinoigakukun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
