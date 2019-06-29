97bd1b3b9f6b3cca592e302f54419a0f2d3da69e
Yuta Saito <kateinoigakukun@gmail.com>
2019-06-29 21:43:29 +0900

    wip

--- KateiSandbox/ViewGraph/Counter.swift
+++ KateiSandbox/ViewGraph/Counter.swift
@@ -9,26 +9,17 @@
 import SwiftUI
 
 struct CounterView: View {
-    
-    var counter = State<Int>(initialValue: 0)
     var hidden = State(initialValue: true)
     
     var body: some View {
         print("render body")
         return VStack {
-            //Text(counter.value.description)
-            //if !hidden.value {
-                CounterButton(
-                    state: 0,//counter.value,
-                    newValue: { _ in
-//                        print("before update: \(self.counter.value), input: \($0)")
-//                        self.counter.value = $0 + 1
-//                        print("after update: \(self.counter.value)")
-                })
-//            }
-//            Toggle(isOn: hidden.binding) {
-//                Text("Hidden")
-//            }
+            if !hidden.value {
+                CounterButton()
+            }
+            Toggle(isOn: hidden.binding) {
+                Text("Hidden")
+            }
         }
     }
 }
@@ -41,25 +32,21 @@ struct CounterState {
 }
 
 struct CounterButton: View, Loggable {
-    let state: State<CounterState>
     let prop = MyDynamicViewProperty()
-    let newValueHandler: (Int) -> Void
-    init(state: Int, newValue: @escaping (Int) -> Void) {
-        self.state = State(initialValue: CounterState(count: state))
-        self.newValueHandler = newValue
-        print("Init with \(state)")
+    let state = State(initialValue: CounterState(count: 0))
+    init() {
+        print("Init button")
     }
 
     var body: some View {
-        print("body with \(state.value.count)")
+        print("Button.body with \(state.value.count)")
         return VStack {
+            Text(state.value.count.description)
             Button(action: {
-            self.state.value.count += 1
-//            self.newValueHandler(self.state.value.count)
-        }) {
-            Text("+")
-        }
-            Text("local: \(self.state.value.count)")
+                self.state.value.count += 1
+            }) {
+                Text("+")
+            }
         }
     }
     static func _makeView(view: _GraphValue<Self>, inputs: _ViewInputs) -> _ViewOutputs {
